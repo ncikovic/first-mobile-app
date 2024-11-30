f915c985df41500d04ef0e6ba8edaf00359104d5
ncikovic <ncikovic@veleri.hr>
2024-11-30 20:18:39 +0100

    izrada prijave i registracije

--- src/pages/PrijavaKorisnikaPage.vue
+++ src/pages/PrijavaKorisnikaPage.vue
@@ -1,11 +1,39 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <h1>Login</h1>
+    <p>Unesite svoje korisničke podatke za prijavu.</p>
+
+    <q-input v-model="username" label="Korisničko ime" />
+    <q-input v-model="password" label="Lozinka" type="password" />
+    <q-btn label="Potvrdi" color="primary" @click="loginUser" />
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
-}
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    loginUser() {
+      // Logika za login, primjer provjere
+      if (this.username && this.password) {
+        // Ako su podaci ispravni, obriši polja i preusmjeri korisnika
+        console.log("Podaci za prijavu:", this.username, this.password);
+
+        // Očisti input polja
+        this.username = "";
+        this.password = "";
+
+        // Preusmjeri korisnika na stranicu Popis Knjiga
+        this.$router.push({ name: "PopisKnjigaPage.vue" }); // Pretpostavka je da ruta ima naziv 'PopisKnjigaPage.vue'
+      } else {
+        alert("Molimo unesite korisničko ime i lozinku.");
+      }
+    },
+  },
+};
 </script>
--- src/pages/RegistracijaKorisnikaPage.vue
+++ src/pages/RegistracijaKorisnikaPage.vue
@@ -1,11 +1,33 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <h1>Registracija</h1>
+    <p>Registrirajte se kako biste dobili pristup našem sistemu.</p>
+
+    <q-input v-model="form.name" label="Ime" />
+    <q-input v-model="form.email" label="Email" type="email" />
+    <q-input v-model="form.username" label="Korisničko ime" />
+    <q-input v-model="form.password" label="Lozinka" type="password" />
+    <q-btn label="Potvrdi" color="primary" @click="registerUser" />
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
-}
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    registerUser() {
+      // Logika za registraciju
+      console.log("Registrirani podaci:", this.form);
+    },
+  },
+};
 </script>
