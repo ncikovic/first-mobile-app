59da3d85d9641f228e039cdf88c7855ccce358f6
ncikovic <ncikovic@veleri.hr>
2024-12-09 10:35:47 +0100

    Dodavanje još događaja u kalendar

--- src/pages/DogadajiPage.vue
+++ src/pages/DogadajiPage.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
-    <q-splitter v-model="splitterModel" style="height: 600px; border-radius: 8px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1)">
+    <q-splitter
+      v-model="splitterModel"
+      style="
+        height: 600px;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+      "
+    >
       <template v-slot:before>
         <div class="q-pa-md map-container">
           <div class="q-mb-md calendar-container">
-            <q-date v-model="date" :events="events" event-color="orange" />
+            <q-date v-model="date" :events="events" event-color="yellow" />
           </div>
           <div id="map" class="map"></div>
         </div>
       </template>
 
       <template v-slot:after>
-        <q-tab-panels v-model="date" animated transition-prev="jump-up" transition-next="jump-up">
+        <q-tab-panels
+          v-model="date"
+          animated
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
           <q-tab-panel v-for="event in events" :name="event" :key="event">
             <div class="event-detail">
               <div class="text-h4 q-mb-md">{{ event }}</div>
-              <p><strong>NAZIV DOGAĐAJA:</strong> {{ eventDetails[event].name }}</p>
-              <p><strong>MJESTO DOGAĐAJA:</strong> {{ eventDetails[event].location }}</p>
-              <p><strong>OPIS:</strong> {{ eventDetails[event].description }}</p>
+              <p>
+                <strong>NAZIV DOGAĐAJA:</strong> {{ eventDetails[event].name }}
+              </p>
+              <p>
+                <strong>MJESTO DOGAĐAJA:</strong>
+                {{ eventDetails[event].location }}
+              </p>
+              <p>
+                <strong>OPIS:</strong> {{ eventDetails[event].description }}
+              </p>
             </div>
           </q-tab-panel>
         </q-tab-panels>
@@ -44,59 +63,101 @@ export default {
       "2024/12/05",
       "2024/12/10",
       "2024/12/15",
+      "2025/01/10",
+      "2025/01/19",
+      "2025/01/20",
+      "2025/02/03",
+      "2025/02/10",
     ];
 
     const eventDetails = {
       "2024/11/05": {
         name: "Veterinarski seminar",
         location: "Zagreb, Park Zrinjevac",
-        description: "Seminar o zdravstvenoj zaštiti kućnih ljubimaca."
+        description: "Seminar o zdravstvenoj zaštiti kućnih ljubimaca.",
       },
       "2024/09/30": {
         name: "Obuka za vlasnike pasa",
         location: "Split, Dioklecijanova palača",
-        description: "Interaktivna radionica za bolje razumijevanje ponašanja pasa."
+        description:
+          "Interaktivna radionica za bolje razumijevanje ponašanja pasa.",
       },
       "2025/01/28": {
         name: "Predavanje o prehrani kućnih ljubimaca",
         location: "Rijeka, Korzo",
-        description: "Stručnjaci dijele savjete o zdravoj prehrani za kućne ljubimce."
+        description:
+          "Stručnjaci dijele savjete o zdravoj prehrani za kućne ljubimce.",
       },
       "2025/05/05": {
         name: "Festival kućnih ljubimaca",
         location: "Osijek, Gradski park",
-        description: "Dan ispunjen zabavnim aktivnostima za ljubimce i njihove vlasnike."
+        description:
+          "Dan ispunjen zabavnim aktivnostima za ljubimce i njihove vlasnike.",
       },
       "2024/12/16": {
         name: "Sajam opreme za kućne ljubimce",
         location: "Zagreb, Velesajam",
-        description: "Prilika za kupnju najnovije opreme i dodataka za ljubimce."
+        description:
+          "Prilika za kupnju najnovije opreme i dodataka za ljubimce.",
       },
       "2025/03/10": {
         name: "Trening spašavanja s psima",
         location: "Varaždin, Dravski park",
-        description: "Demonstracija i obuka pasa za spašavanje u hitnim situacijama."
+        description:
+          "Demonstracija i obuka pasa za spašavanje u hitnim situacijama.",
       },
       "2025/06/22": {
         name: "Maraton s ljubimcima",
         location: "Zadar, Riva",
-        description: "Humanitarna utrka s kućnim ljubimcima za pomoć napuštenim životinjama."
+        description:
+          "Humanitarna utrka s kućnim ljubimcima za pomoć napuštenim životinjama.",
       },
       "2024/12/05": {
         name: "Pet Fashion Show",
         location: "Zagreb, Trg bana Jelačića",
-        description: "Prikaz najnovijih modnih trendova za kućne ljubimce."
+        description: "Prikaz najnovijih modnih trendova za kućne ljubimce.",
       },
       "2024/12/10": {
         name: "Božićna zabava za kućne ljubimce",
         location: "Osijek, Gradski park",
-        description: "Zabava u kojoj se vlasnici kućnih ljubimaca mogu natjecati u raznim disciplinama sa svojim ljubimcima."
+        description:
+          "Zabava u kojoj se vlasnici kućnih ljubimaca mogu natjecati u raznim disciplinama sa svojim ljubimcima.",
       },
       "2024/12/15": {
         name: "Predavanje o očuvanju zdravlja pasa",
         location: "Rijeka, Kulturni centar",
-        description: "Predavanje stručnjaka o važnosti preventivnih zdravstvenih pregleda i pravilne brige za pse."
-      }
+        description:
+          "Predavanje stručnjaka o važnosti preventivnih zdravstvenih pregleda i pravilne brige za pse.",
+      },
+      "2025/01/10": {
+        name: "Winter Pet Walk",
+        location: "Pula, Park Verudela",
+        description:
+          "Zimska šetnja za kućne ljubimce i njihove vlasnike s nagradama za najljepše obučene ljubimce.",
+      },
+      "2025/01/19": {
+        name: "Zdravlje kućnih ljubimaca – radionica",
+        location: "Split, Sportska dvorana",
+        description:
+          "Radionica o prevenciji bolesti i pravilnoj prehrani kućnih ljubimaca.",
+      },
+      "2025/01/20": {
+        name: "Psi na ledu - natjecanje",
+        location: "Zadar, Zimski kompleks",
+        description: "Natjecanje pasa u sportskim disciplinama na ledu. ",
+      },
+      "2025/02/03": {
+        name: "Izložba pasa i mačaka",
+        location: "Varaždin, Sajamski centar",
+        description:
+          "Izložba različitih pasmina pasa i mačaka, natjecanje u ljepoti i poslušnosti.",
+      },
+      "2025/02/10": {
+        name: "Trening za pse spasioce",
+        location: "Osijek, Gradski centar",
+        description:
+          "Obuka za treniranje pasa za hitne situacije i intervencije.",
+      },
     };
 
     onMounted(() => {
